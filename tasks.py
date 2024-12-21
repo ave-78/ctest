976979f8e6b009efa12bddf9af583ac2e0325ae8
Alexey Elistratov <ave-78@mail.ru>
2024-12-21 19:46:32 +0300

    task func

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from celery import Celery, chain, group
+from celery import Celery  # , chain, group
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 
@@ -8,26 +8,26 @@ def add(x, y):
     return x + y
 
 
-@app.task
-def prep():
-    nt = "NIOSS_DATA"
-    return nt
-
-
-@app.task
-def single(a, b="B", c="C", nt="NO_NIOSS"):
-    res=f"{a} {b=} {c=} {nt=}"
-    return res
-
-
-@app.task
-def post(res):
-    print("final", res)
-    return res
-
-
-@app.task
-def func():
-    it = [(n, f"b{n}", f"c{n}") for n in range(10)]
-    pipe = chain(prep.s(), group(single(a, b, c) for (a, b, c) in it), post.s())
-    return pipe
+# @app.task
+# def prep():
+#     nt = "NIOSS_DATA"
+#     return nt
+#
+#
+# @app.task
+# def single(a, b="B", c="C", nt="NO_NIOSS"):
+#     res=f"{a} {b=} {c=} {nt=}"
+#     return res
+#
+#
+# @app.task
+# def post(res):
+#     print("final", res)
+#     return res
+#
+#
+# @app.task
+# def func():
+#     it = [(n, f"b{n}", f"c{n}") for n in range(10)]
+#     pipe = chain(prep.s(), group(single(a, b, c) for (a, b, c) in it), post.s())
+#     return pipe
