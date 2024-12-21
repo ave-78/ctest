79b68d927664c90583e8a89c4d3e0cf82e1cdd43
Alexey Elistratov <ave-78@mail.ru>
2024-12-21 19:36:15 +0300

    task func

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery import Celery, chain, group
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 
@@ -6,3 +6,28 @@ app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 @app.task
 def add(x, y):
     return x + y
+
+
+@app.task
+def prep():
+    nt = "NIOSS_DATA"
+    return nt
+
+
+@app.task
+def single(a, b="B", c="C", nt="NO_NIOSS"):
+    res=f"{a} {b=} {c=} {nt=}"
+    return res
+
+
+@app.task
+def post(res):
+    print("final", res)
+    return res
+
+
+@app.task
+def func():
+    it = [(n, f"b{n}", f"c{n}") for n in range(10)]
+    pipe = chain(prep.s(), group(single(a, b, c) for (a, b, c) in it), post.s())
+    return pipe
