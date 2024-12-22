ce787a84cbc5fa80560884c61c49fce62cb3d90f
Alexey Elistratov <ave-78@mail.ru>
2024-12-22 13:09:22 +0300

    task func

--- tasks.py
+++ tasks.py
@@ -9,6 +9,22 @@ def add(x, y):
 
 
 @app.task
+def mul(x, y):
+    return x * y
+
+
+@app.task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@app.task
+def func2():
+    res = chain(add.s(4, 4) | mul.s(8))()
+    return res
+
+
+@app.task
 def prep():
     nt = "NIOSS_DATA"
     return nt
@@ -29,5 +45,5 @@ def post(res):
 @app.task
 def func1():
     it = [(n, f"b{n}", f"c{n}") for n in range(10)]
-    pipe = chain(prep.s() | group(single.s(a, b, c) for (a, b, c) in it) | post.s())
+    pipe = chain(prep.s() | group(single.s(a, b, c) for (a, b, c) in it) | post.s())()
     return pipe
